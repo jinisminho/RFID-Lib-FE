6e79ceb5df9eff16f314d766e8e7b8319db1e14d
Khang Nguyen <khang090999@gmail.com>
2021-02-28 21:04:46 +0700

    [khangndn] book status

--- LibraryWebApp/src/views/Util/Constant.js
+++ LibraryWebApp/src/views/Util/Constant.js
@@ -58,8 +58,8 @@ export const CURRENCY = "VND"
 
 
 //Book status
-export const BOOK_STATUS_ADD_LIST = { OUT_OF_CIRCULATION: "Out of circulation", IN_CIRCULATION: "In circulation", LIB_USE_ONLY: "Library use only" }
-export const BOOK_STATUS_LIST = { OUT_OF_CIRCULATION: "Out of circulation", IN_CIRCULATION: "In circulation", LIB_USE_ONLY: "Library use only", DISCARD: "Discard" }
+export const BOOK_STATUS_ADD_LIST = { IN_CIRCULATION: "In circulation", OUT_OF_CIRCULATION: "Out of circulation", LIB_USE_ONLY: "Library use only" }
+export const BOOK_STATUS_LIST = { IN_CIRCULATION: "In circulation", OUT_OF_CIRCULATION: "Out of circulation", LIB_USE_ONLY: "Library use only", DISCARD: "Discard" }
 
 export const BOOK_COPY_STATUS_LIST = { AVAILABLE: "Available", IN_PROCESS: "In process", BORROWED: "Borrowed", OUT_OF_CIRCULATION: "Out of circulation", LOST: "Lost", LIB_USE_ONLY: "Library use only", DISCARD: "Discard" }
 
