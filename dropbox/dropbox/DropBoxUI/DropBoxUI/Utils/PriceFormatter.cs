9f12bae82abc12fffff657d4b15df0f9f0a4c5a3
Tram Phan <hoang.tram2908@gmail.com>
2021-03-31 17:28:49 +0700

    [WIN] format price

---
+++ dropbox/dropbox/DropBoxUI/DropBoxUI/Utils/PriceFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropBoxUI.Utils
+{
+    public static class PriceFormatter
+    {
+        public static String formatToDecimal(double price)
+        {
+            if (price == (long)price)
+                return String.Format("%d", (long)price);
+            else
+                return String.Format("%s", price);
+        }
+    }
+}
