caddde7fad4a2f082d817cdd91d5f88c05534a65
KienNT <knguyen.05.07.97@gmail.com>
2021-04-02 11:35:12 +0700

    [FE] Modified min number of borrow policy and patron type from 1 to 0

--- LibraryWebApp/src/components/Forms/AddBorrowPolicyForm.js
+++ LibraryWebApp/src/components/Forms/AddBorrowPolicyForm.js
@@ -100,8 +100,8 @@ const validate = values => {
 }
 
 const validateNumber = value => {
-    if (value < 1) {
-        return 1
+    if (value < 0) {
+        return 0
     } else {
         return value
     }
