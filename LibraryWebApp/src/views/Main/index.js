a8a0a5a68a5369adddcb74811390a066c9161a62
Khang Nguyen <khang090999@gmail.com>
2021-04-12 21:28:17 +0700

    [khangndn] auto focus tag book copy, fix authen

--- LibraryWebApp/src/views/Main/index.js
+++ LibraryWebApp/src/views/Main/index.js
@@ -26,16 +26,7 @@ class Main extends Component {
   componentDidMount() {
     this.props.onTryAutoSignUp()
   }
-  deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-  }
+ 
   render() {
 
     let display = (
@@ -73,7 +64,10 @@ class Main extends Component {
         )
       }
        else {
-        this.deleteAllCookies()
+        localStorage.removeItem("Userid")
+            localStorage.removeItem("Role")
+            localStorage.removeItem("Avatar")
+            localStorage.removeItem("Username")
         this.props.onLogout()
       }
     }
